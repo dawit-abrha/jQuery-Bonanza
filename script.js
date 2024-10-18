e6a18348e04b4f27ab4fe4d6244654b7fdaa5e3d
Your name <your.email@yourmailserver.com>
2024-10-18 13:52:10 +0200

    jQuery Bonanza assignment 1

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $(".fad").click(function(){
+        $(".par1").fadeOut();
+    })
+   });
+   $(document).ready(function(){
+    $(".Append").click(function(){
+        $(".add").append("<p>my name is dawit</p>")
+
+
+    })
+   
+   })
+   $(document).ready(function(){
+    $(".chenge").click(function(){
+        $(".clr").css("color", "red");
+      });
+   
+   })
